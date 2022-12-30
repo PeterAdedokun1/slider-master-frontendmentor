2b2cf92f0bddbe0791b082faa4681453b20039fe
Adedokun peter <peteradedokun436@gmail.com>
2022-12-30 23:54:45 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { data } from "./data"
+import prevBtn from "./images/icon-prev.svg"
+import nextBtn from "./images/icon-next.svg"
 
 function App() {
+  const [state, setState] = useState(0);
+  const { id, image, name, job, text } = data[state]
+  console.log(state)
+  const checkIndex = (prop) => {
+    if (prop > data.length-1) {
+      return 0
+    } if (prop < 0) {
+      return data.length - 1;
+    }
+    return prop
+  }
+  const nextButton = () => {
+    setState((index) => {
+      return checkIndex(index+1)
+    })
+  }
+  const previousBtn = () => {
+    setState((index) => {
+      return checkIndex(index - 1);
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="containerr">
+      <div className="word">
+        <p className="text">{text}</p>
+        <div className="text-sec">
+          <h4>{name}</h4>
+          <p className="job">{job}</p>    
+        </div>
+      </div>
+      <div className="img-container">
+        <img className="picture" src={image} alt="" />
+        <div className="btn">
+         <img src={prevBtn} onClick={previousBtn} alt="" className="next-btn"/>
+        <img src={nextBtn} alt="" onClick={nextButton} />
+       </div>
+       
+      </div>
+      
     </div>
   );
 }
